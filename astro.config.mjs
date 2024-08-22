98727600837a6e26f56279e7c29730ce11641140
thisisabhinay <50246894+thisisabhinay@users.noreply.github.com>
2024-08-22 14:12:24 +0530

    build: setup tailwindcss

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,27 +1,30 @@
 import starlight from '@astrojs/starlight';
+import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    starlight({
-      title: "React Recipes",
-      social: {
-        github: "https://github.com/thisisabhinay/react-recipes",
-      },
-      sidebar: [
-        {
-          label: "React Hooks",
-          items: [
-            // Each item here is one entry in the navigation menu.
-            { label: "Effect Logger", slug: "guides/effect-logger" },
-          ],
-        },
-        {
-          label: "Reference",
-          autogenerate: { directory: "reference" },
-        },
-      ],
-    }),
-  ],
-})
+	integrations: [
+		starlight({
+			title: 'Docs with Tailwind',
+			social: {
+				github: 'https://github.com/withastro/starlight',
+			},
+			sidebar: [
+				{
+					label: 'Guides',
+					items: [
+						// Each item here is one entry in the navigation menu.
+						{ label: 'Example Guide', slug: 'guides/example' },
+					],
+				},
+				{
+					label: 'Reference',
+					autogenerate: { directory: 'reference' },
+				},
+			],
+			customCss: ['./src/tailwind.css'],
+		}),
+		tailwind({ applyBaseStyles: false }),
+	],
+});
--- src/content/docs/guides/example.md
+++ src/content/docs/guides/example.md
@@ -1,5 +1,5 @@
 ---
-title: Effect Logger
+title: Example Guide
 description: A guide in my new Starlight docs site.
 ---
 
