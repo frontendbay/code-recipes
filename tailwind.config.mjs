c2925fd0c51f4b0fe77866022be33beba3b2c0c3
thisisabhinay <50246894+thisisabhinay@users.noreply.github.com>
2024-08-29 17:30:21 +0530

    fix: dark mode sync with tailwind classes

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,6 +4,7 @@ import colors from 'tailwindcss/colors';
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  darkMode: ["selector", '[data-theme="dark"]'],
   theme: {
     extend: {
       colors: {
