c0714e09ed83f56e1977198b78d93c5d1c629b1e
thisisabhinay <50246894+thisisabhinay@users.noreply.github.com>
2024-09-09 14:12:37 +0530

    fix: update index page to include categories from design engineering

--- src/config/sidebar-context.ts
+++ src/config/sidebar-context.ts
@@ -9,6 +9,11 @@ const sidebarContext = [
     name: "react",
     href: "/react/getting-started/introduction",
   },
+  {
+    title: "Design Engineering",
+    name: "design-engineering",
+    href: "/design-engineering/getting-started/introduction",
+  },
 ]
 
 export default sidebarContext
