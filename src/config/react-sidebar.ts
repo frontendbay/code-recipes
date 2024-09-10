b6fc95b7c57ce308fb920ae7218ea39a054b0269
thisisabhinay <50246894+thisisabhinay@users.noreply.github.com>
2024-09-10 19:42:14 +0530

    fix: reorganize react recipes and create overview pages

--- src/config/react-sidebar.ts
+++ src/config/react-sidebar.ts
@@ -1,59 +1,48 @@
 const reactSidebar = [
   {
-    label: "Getting Strated",
-    autogenerate: { directory: "react/getting-started" },
-  },
-  {
-    label: "React Hooks",
-    autogenerate: { directory: "react/react-hooks" },
-  },
-  {
-    label: "Effects Recipes",
-    autogenerate: { directory: "react/effects-recipes" },
-  },
-  {
     label: "Custom Hooks",
     autogenerate: { directory: "react/custom-hooks" },
-  },
-  {
-    label: "React Recipes",
-    autogenerate: { directory: "react/react-recipes" },
-  },
-  {
-    label: "Redux Recipes",
-    autogenerate: { directory: "react/redux-recipes" },
-  },
-  {
-    label: "React Router Recipes",
-    autogenerate: { directory: "react/react-router-recipes" },
-  },
-  {
-    label: "React Web Workers Recipes",
-    autogenerate: { directory: "react/react-worker-recipes" },
-  },
-  {
-    label: "Request Animation Recipes",
-    autogenerate: { directory: "react/request-animation-recipes" },
-  },
-  {
-    label: "Testing Recipes",
-    autogenerate: { directory: "react/testing-recipes" },
-  },
-  {
-    label: "Performance Optimization",
-    autogenerate: { directory: "react/performance-optimization" },
   },
   {
     label: "Design Patterns",
     autogenerate: { directory: "react/design-patterns" },
   },
   {
+    label: "Effects Recipes",
+    autogenerate: { directory: "react/effects-recipes" },
+  },
+  {
+    label: "Getting Strated",
+    autogenerate: { directory: "react/getting-started" },
+  },
+
+  {
+    label: "Performance Optimization",
+    autogenerate: { directory: "react/performance-optimization" },
+  },
+  {
+    label: "React Hooks",
+    autogenerate: { directory: "react/react-hooks" },
+  },
+  {
+    label: "React Recipes",
+    autogenerate: { directory: "react/react-recipes" },
+  },
+  {
+    label: "React Router",
+    autogenerate: { directory: "react/react-router-recipes" },
+  },
+  {
+    label: "Redux Recipes",
+    autogenerate: { directory: "react/redux-recipes" },
+  },
+  {
     label: "Render Patterns",
     autogenerate: { directory: "react/render-patterns" },
   },
   {
-    label: "Reference",
-    autogenerate: { directory: "react/reference" },
+    label: "State Management",
+    autogenerate: { directory: "react/state-management" },
   },
 ]
 
