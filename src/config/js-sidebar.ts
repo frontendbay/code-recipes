c271874c9eb93d380b52128619936879aa2fa628
thisisabhinay <50246894+thisisabhinay@users.noreply.github.com>
2024-09-10 18:16:50 +0530

    fix: create overview and boilerplate recipes pages for javascript

--- src/config/js-sidebar.ts
+++ src/config/js-sidebar.ts
@@ -3,6 +3,14 @@ const jsSidebar = [
     label: "Getting Started",
     autogenerate: { directory: "javascript/getting-started" },
   },
+  {
+    label: "Functional Programming",
+    autogenerate: { directory: "javascript/functional-programming" },
+  },
+  {
+    label: "DOM",
+    autogenerate: { directory: "javascript/dom" },
+  },
 ]
 
 export default jsSidebar
