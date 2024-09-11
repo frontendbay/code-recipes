9e7ee1da2cfcf09624d92ddb766e80d1e8288cbf
thisisabhinay <50246894+thisisabhinay@users.noreply.github.com>
2024-09-11 19:31:48 +0530

    fix: added more social media links

--- src/config/social-links.ts
+++ src/config/social-links.ts
@@ -1,5 +1,7 @@
 const socialLinks = {
   github: "https://github.com/thisisabhinay/frontend-recipes",
+  twitter: "https://x.com/@thisisabhinay",
+  linkedin: "https://www.linkedin.com/services/page/402905330a57085581/",
 }
 
 export default socialLinks
