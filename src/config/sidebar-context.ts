909a773b14f7badddb51c8a1d9c787820bdfd37e
Abhinay Thakur <a8h1nay@gmail.com>
2024-09-17 15:05:07 +0530

    fix: add icons in the dropdowns

--- src/config/sidebar-context.ts
+++ src/config/sidebar-context.ts
@@ -3,31 +3,37 @@ const sidebarContext = [
     title: "JavaScript",
     name: "javascript",
     href: "/javascript/getting-started/introduction",
+    icon: "https://skillicons.dev/icons?i=js",
   },
   {
     title: "React",
     name: "react",
     href: "/react/getting-started/introduction",
+    icon: "https://skillicons.dev/icons?i=react",
   },
   {
     title: "Design Engineering",
     name: "design-engineering",
     href: "/design-engineering/getting-started/introduction",
+    icon: "https://skillicons.dev/icons?i=tailwind",
   },
   {
     title: "Build Tools",
     name: "build-tools",
     href: "/build-tools/getting-started/introduction",
+    icon: "https://skillicons.dev/icons?i=webpack",
   },
   {
     title: "Unit Testing",
     name: "unit-testing",
     href: "/unit-testing/getting-started/introduction",
+    icon: "https://skillicons.dev/icons?i=jest",
   },
   {
     title: "Micro Frontends",
     name: "micro-frontends",
     href: "/micro-frontends/getting-started/introduction",
+    icon: "https://skillicons.dev/icons?i=sequelize",
   },
 ]
 
