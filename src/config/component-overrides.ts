4d60a1189c2b31fe10cb79b8663845f6d0527925
Abhinay Thakur <a8h1nay@gmail.com>
2024-09-21 21:16:17 +0530

    fix: overriding Hero component

--- src/config/component-overrides.ts
+++ src/config/component-overrides.ts
@@ -2,6 +2,7 @@ const overridenComponents = {
   PageSidebar: "./src/components/starlight/PageSidebar.astro",
   MarkdownContent: "./src/components/starlight/MarkdownContent.astro",
   Sidebar: "./src/components/starlight/Sidebar.astro",
+  Hero: "./src/components/starlight/Hero.astro",
 }
 
 export default overridenComponents
